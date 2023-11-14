4e4a06ee44e767b804efefa4b2fd54cbfae8c21d
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-13 23:48:01 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('screenshot')
+def handle_screenshot(data):
+    socketio.emit('screenshot', data)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ server.php
@@ -0,0 +1,45 @@
+// server.php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+require __DIR__ . '/vendor/autoload.php';
+
+class ScreenShare implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            $client->send($msg);
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new ScreenShare()
+        )
+    ),
+    8080
+);
+
+$server->run();
